d0855ef66077a434903cda3bfb96a6719b4dec08
John Snyder <johnsnyder7@gmail.com>
2017-11-20 12:36:35 -0500

    Initial commit

---
+++ spec/arrow-debug-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ArrowDebugView from '../lib/arrow-debug-view';
+
+describe('ArrowDebugView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
